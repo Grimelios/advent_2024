c5b5cfe87cb8408729415f996c4e5ebb81483844
Mark Hatfield <grimelios@gmail.com>
2024-12-09 23:14:32 -0600

    Day 10.1

---
+++ day10.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+def recurse(x, y, current):
+	if current == 9:
+		key = (x, y)
+
+		if key in covered:
+			return 0
+
+		covered.add(key)
+
+		return 1
+
+	sum_ = 0
+
+	if x > 0 and topological[y][x - 1] == current + 1:
+		sum_ += recurse(x - 1, y, current + 1)
+
+	if x < w - 1  and topological[y][x + 1] == current + 1:
+		sum_ += recurse(x + 1, y, current + 1)
+
+	if y > 0 and topological[y - 1][x] == current + 1:
+		sum_ += recurse(x, y - 1, current + 1)
+
+	if y < h - 1 and topological[y + 1][x] == current + 1:
+		sum_ += recurse(x, y + 1, current + 1)
+
+	return sum_
+
+with open('day10.txt') as f:
+	topological = [[int(c) for c in line.strip()] for line in f]
+
+w = len(topological[0])
+h = len(topological)
+sum_ = 0
+
+for y in range(h):
+	for x in range(w):
+		if topological[y][x] == 0:
+			covered = set()
+			sum_ += recurse(x, y, 0)
+
+print(sum_)
